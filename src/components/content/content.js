4e4d2bc799c1cc29548d49a93b08abeb0a78067c
Nikita Budiakov <budyakov91@gmail.com>
2020-03-07 18:00:55 +0300

    content block

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -4,11 +4,11 @@ import OnSale from "./on-sale/on-sale";
 import NewListings from "./new-listings/new-listings";
 import News from "./news/news";
 
-import style from "./content.scss";
+import "./content.scss";
 
 const Content = () => {
     return (
-        <div className={style.content}>
+        <div className="content">
             <Filter/>
             <OnSale/>
             <NewListings/>
