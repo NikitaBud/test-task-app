1cd362815bdb1bce87f2ee8627b98d137e3d9c56
Nikita Budiakov <budyakov91@gmail.com>
2020-03-07 18:00:27 +0300

    base structure and style filter-component

--- src/components/content/filter/filter.js
+++ src/components/content/filter/filter.js
@@ -1,9 +1,34 @@
 import React from "react";
 
+import "./filter.scss";
+
 const Filter = () => {
     return (
         <div>
-            Filter
+            <h2>Filter</h2>
+            <ul className="filter-items">
+                <li className="filter-item">
+                    <button className="btn btn-filter" type="button">By Algorithm</button>
+                </li>
+                <li className="filter-item">
+                    <button className="btn btn-filter" type="button">By Coin</button>
+                </li>
+                <li className="filter-item">
+                    <button className="btn btn-filter" type="button">By Equipment</button>
+                </li>
+                <li className="filter-item">
+                    <button className="btn btn-filter" type="button">By Manufacturer</button>
+                </li>
+                <li className="filter-item">
+                    <button className="btn btn-filter" type="button">Minimum price</button>
+                </li>
+                <li className="filter-item">
+                    <button className="btn btn-filter" type="button">Maximum price</button>
+                </li>
+                <li className="filter-item">
+                    <button className="btn btn-filter" type="button">Search</button>
+                </li>
+            </ul>
         </div>
     )
 }
